95d1cb8a5a04a4f59a8c56463319753f6c9f0435
Nicholas Tassone <ntassone@gmail.com>
2014-01-14 11:23:19 -0400

    responsive bits

--- html/js/main.js
+++ html/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
+
+  //Menu Toggle
+  var $menu = $('.main-nav ul'),
+      $menulink = $('.menu-toggle');
   
+  $menulink.click(function() {
+    $menulink.toggleClass('active');
+    $menu.toggleClass('active');
+    return false;
+  });
+
 });
